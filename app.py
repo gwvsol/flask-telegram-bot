221efa978395022f122eaf5ce09e8ba0eea2edbf
gwvsol <gwvsol@gmail.com>
2019-02-06 23:43:02 +0200

    created application menu

--- app.py
+++ app.py
@@ -26,6 +26,72 @@ WEBHOOK_SSL_CERT = app.config['WEBHOOK_SSL_CERT']
 WEBHOOK_SSL_PRIV = app.config['WEBHOOK_SSL_PRIV']
 
 
+#===================== Bot Telegram ====================================
+@bot.message_handler(commands=['start'])
+def handle_text(message):
+    #print(message.chat.id)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['CREATE_USER'])
+    user_markup.row(app.config['EDIT_PROFILE'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
+    bot.send_message(message.from_user.id, \
+    app.config['SELECT_MENU'], reply_markup=user_markup)
+
+
+@bot.message_handler(func=lambda mess: app.config['MAIN_MENU'] == mess.text, content_types=['text'])
+def main_menu(message):
+    #print(message.chat.id)
+    #print(message.text)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['CREATE_USER'])
+    user_markup.row(app.config['EDIT_PROFILE'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK']) 
+    bot.send_message(message.from_user.id, app.config['SELECT_MENU'], reply_markup=user_markup)
+
+
+@bot.message_handler(func=lambda mess: app.config['CREATE_USER'] == mess.text, content_types=['text'])
+def main_menu(message):
+    #print(message.chat.id)
+    #print(message.text)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
+    bot.send_message(message.from_user.id, \
+    app.config['NEW_USER'], reply_markup=user_markup)
+
+
+@bot.message_handler(func=lambda mess: app.config['EDIT_PROFILE'] == mess.text, content_types=['text'])
+def main_menu(message):
+    #print(message.chat.id)
+    #print(message.text)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
+    bot.send_message(message.from_user.id, \
+    app.config['LOGIN'], reply_markup=user_markup)
+
+
+@bot.message_handler(func=lambda mess: app.config['UPDATES'] == mess.text, content_types=['text'])
+def handle_text(message):
+    #print(message.chat.id)
+    #print(message.text)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    bot.send_message(message.chat.id, \
+    app.config['UPDATE'], reply_markup=user_markup)
+
+
+@bot.message_handler(func=lambda mess: app.config['FEEDBACK'] == mess.text, content_types=['text'])
+def handle_text(message):
+    #print(message.chat.id)
+    #print(message.text)
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    bot.send_message(message.chat.id, \
+    app.config['QUESTIONS'], reply_markup=user_markup)
+
+
+#============================= Flask =================================
 # Process webhook calls
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
@@ -37,23 +103,6 @@ def webhook():
     else:
         flask.abort(403)
 
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 ("Привет, Я EchoBot.\n"
-                  "Я могу повторить все ваши слова."))
-
-
-# Handle all other messages
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-    
-#============================================    
-
-#=================================
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
 bot.remove_webhook()
--- config.py
+++ config.py
@@ -1,7 +1,24 @@
 class Config(object):
+#===== Set Bot Telegram =======
     API_TOKEN = '742304158:AAFeunhPvTtDs7D4bJ8OG1XU3iJPVNyBbTY'
     WEBHOOK_HOST = 'srv00.hldns.ru'
     WEBHOOK_PORT = 8443                    # 443, 80, 88 or 8443 (port need to be 'open')
     WEBHOOK_LISTEN = '0.0.0.0'
     WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Path to the ssl certificate
     WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Path to the ssl private key
+#===== Set Data Base ==========
+    DB_CONFIG = { 'host': 'localhost', 'port': 28015, }
+    DB_NAME = 'bot'
+    DB_TAB = {'tab_1': 'chat_log', 'tab_2': 'app_log'}
+    DB_CONT = {'login': None, 'name': None, 'passw': None, 'gender': None, 'phone': None, 'email': None}
+#===== Message ================
+    CREATE_USER = 'Создание нового пользователя'
+    EDIT_PROFILE = 'Редактирование профиля'
+    UPDATES = 'Обновления'
+    FEEDBACK = 'Обратная связь'
+    MAIN_MENU = 'Главное меню'
+    SELECT_MENU = 'Выберите пункт меню:'
+    NEW_USER = 'Login нового пользователя:'
+    LOGIN = 'Login пользователя:'
+    UPDATE = 'Обновления и дополнительная информация: https://t.me/flaskbott'
+    QUESTIONS = 'Вопросы и предложения:\n • @jwvsolmf\n • jwvsol@yandex.com'
