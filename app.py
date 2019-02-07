0d8983bd3c10977d0e9a27b831addb12c32da59e
gwvsol <gwvsol@gmail.com>
2019-02-07 10:49:39 +0200

    added a new menu and work with the database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request, abort
-from rethinkdbcm import UseDatabase
 from hashlib import md5
 from datetime import datetime
 from config import Config
@@ -18,22 +18,30 @@ app = Flask(__name__)
 app.config.from_object(Config)
 bot = telebot.TeleBot(app.config['API_TOKEN'])
 
-
+#============ Bot ===============================
 WEBHOOK_PORT = app.config['WEBHOOK_PORT']
 WEBHOOK_LISTEN = app.config['WEBHOOK_LISTEN']
 WEBHOOK_URL_BASE = 'https://{}:{}'.format(app.config['WEBHOOK_HOST'], app.config['WEBHOOK_PORT'])
 WEBHOOK_URL_PATH = '/%s/'.format(app.config['API_TOKEN'])
 WEBHOOK_SSL_CERT = app.config['WEBHOOK_SSL_CERT']
 WEBHOOK_SSL_PRIV = app.config['WEBHOOK_SSL_PRIV']
+#============ Data Base =========================
+DB_CONF = app.config['DB_CONFIG']
+DB_BOT = app.config['DB_NAME']['db_bot']
+DB_API = app.config['DB_NAME']['db_api']
+TAB_CHAT = app.config['TAB_BOT']['chat_log']
+TAB_API = app.config['TAB_API']['data']
+TAB_ROOT = app.config['TAB_BOT']['root']
+#================================================
 
 
 #===================== Bot Telegram ====================================
 @bot.message_handler(commands=['start'])
 def handle_text(message):
-    #print(message.chat.id)
     user_markup = types.ReplyKeyboardMarkup(True, False)
-    user_markup.row(app.config['CREATE_USER'])
-    user_markup.row(app.config['EDIT_PROFILE'])
+    user_markup.row(app.config['USER_DATA'])
+    user_markup.row(app.config['CREATE_USER'], app.config['EDIT_PROFILE'])
+    user_markup.row(app.config['SETTING'], app.config['PASSW_RECOV'])
     user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
     bot.send_message(message.from_user.id, \
     app.config['SELECT_MENU'], reply_markup=user_markup)
@@ -41,19 +49,25 @@ def handle_text(message):
 
 @bot.message_handler(func=lambda mess: app.config['MAIN_MENU'] == mess.text, content_types=['text'])
 def main_menu(message):
-    #print(message.chat.id)
-    #print(message.text)
     user_markup = types.ReplyKeyboardMarkup(True, False)
-    user_markup.row(app.config['CREATE_USER'])
-    user_markup.row(app.config['EDIT_PROFILE'])
+    user_markup.row(app.config['USER_DATA'])
+    user_markup.row(app.config['CREATE_USER'], app.config['EDIT_PROFILE'])
+    user_markup.row(app.config['SETTING'], app.config['PASSW_RECOV'])
     user_markup.row(app.config['UPDATES'], app.config['FEEDBACK']) 
-    bot.send_message(message.from_user.id, app.config['SELECT_MENU'], reply_markup=user_markup)
+    bot.send_message(message.from_user.id, app.config['SELECT_MENU'], reply_markup=user_markup) 
+    
 
+@bot.message_handler(func=lambda mess: app.config['USER_DATA'] == mess.text, content_types=['text'])
+def main_menu(message):
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
+    bot.send_message(message.from_user.id, \
+    app.config['LOGIN'], reply_markup=user_markup)
+    
 
 @bot.message_handler(func=lambda mess: app.config['CREATE_USER'] == mess.text, content_types=['text'])
 def main_menu(message):
-    #print(message.chat.id)
-    #print(message.text)
     user_markup = types.ReplyKeyboardMarkup(True, False)
     user_markup.row(app.config['MAIN_MENU'])
     user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
@@ -63,8 +77,6 @@ def main_menu(message):
 
 @bot.message_handler(func=lambda mess: app.config['EDIT_PROFILE'] == mess.text, content_types=['text'])
 def main_menu(message):
-    #print(message.chat.id)
-    #print(message.text)
     user_markup = types.ReplyKeyboardMarkup(True, False)
     user_markup.row(app.config['MAIN_MENU'])
     user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
@@ -72,10 +84,26 @@ def main_menu(message):
     app.config['LOGIN'], reply_markup=user_markup)
 
 
+@bot.message_handler(func=lambda mess: app.config['PASSW_RECOV'] == mess.text, content_types=['text'])
+def main_menu(message):
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
+    bot.send_message(message.from_user.id, \
+    app.config['EMAIL'], reply_markup=user_markup)
+
+#================= Меню Настройка приложения ================================
+@bot.message_handler(func=lambda mess: app.config['SETTING'] == mess.text, content_types=['text'])
+def main_menu(message):
+    user_markup = types.ReplyKeyboardMarkup(True, False)
+    user_markup.row(app.config['MAIN_MENU'])
+    user_markup.row(app.config['UPDATES'], app.config['FEEDBACK'])
+    bot.send_message(message.from_user.id, \
+    app.config['LOGIN'], reply_markup=user_markup)
+
+#================== О приложении, как связяться ========================
 @bot.message_handler(func=lambda mess: app.config['UPDATES'] == mess.text, content_types=['text'])
 def handle_text(message):
-    #print(message.chat.id)
-    #print(message.text)
     user_markup = types.ReplyKeyboardMarkup(True, False)
     user_markup.row(app.config['MAIN_MENU'])
     bot.send_message(message.chat.id, \
@@ -84,15 +112,13 @@ def handle_text(message):
 
 @bot.message_handler(func=lambda mess: app.config['FEEDBACK'] == mess.text, content_types=['text'])
 def handle_text(message):
-    #print(message.chat.id)
-    #print(message.text)
     user_markup = types.ReplyKeyboardMarkup(True, False)
     user_markup.row(app.config['MAIN_MENU'])
     bot.send_message(message.chat.id, \
     app.config['QUESTIONS'], reply_markup=user_markup)
 
 
-#============================= Flask =================================
+#============================= Flask ===================================
 # Process webhook calls
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from enum import Enum
 
 class Config(object):
@@ -8,28 +9,37 @@ class Config(object):
     WEBHOOK_LISTEN = '0.0.0.0'
     WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Path to the ssl certificate
     WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Path to the ssl private key
+    ROOT_USER = {'login': 'root', 'passw': 'b4b8daf4b8ea9d39568719e1e320076f'}
 #===== Set Data Base ==========
     DB_CONFIG = { 'host': 'localhost', 'port': 28015, }
-    DB_NAME = 'bot'
-    DB_TAB = {'tab_1': 'chat_log', 'tab_2': 'app_log'}
-    DB_CONT = {'id': None, 'login': None, 'name': None, 'passw': None, 'gender': None, 'phone': None, 'email': None}
-#===== Message ================
-    CREATE_USER = 'Создание нового пользователя'
-    EDIT_PROFILE = 'Редактирование профиля'
-    UPDATES = 'Обновления'
-    FEEDBACK = 'Обратная связь'
+    DB_NAME = {'db_bot': 'db_bot', 'db_api': 'data'}
+    TAB_BOT = {'chat_log': 'chat_log', 'app_log': 'app_log', 'root': 'root'}
+    TAB_API = {'data': 'data', 'log': 'log', 'root': 'root'}
+    USER_PERMISSION = None
+    DB_CONT = {'login': None, 'name': None, 'passw': None, 'gender': None, 'phone': None, 'email': None}
+#========= Menu ===============
+    USER_DATA = 'Данные пользователя'
+    CREATE_USER = 'Создание'
+    EDIT_PROFILE = 'Редактирование'
+    PASSW_RECOV = 'Восстановление'
+    SETTING = 'Настройка'
+    UPDATES = 'О приложении'
+    FEEDBACK = 'Как связаться'
     MAIN_MENU = 'Главное меню'
+#===== Message ================
     SELECT_MENU = 'Выберите пункт меню:'
     NEW_USER = 'Login нового пользователя:'
-    LOGIN = 'Login пользователя:'
+    LOGIN = 'Login:'
+    EMAIL = 'Введите номер телефона:'
     UPDATE = 'Обновления и дополнительная информация: https://t.me/flaskbott'
     QUESTIONS = 'Вопросы и предложения:\n • @jwvsolmf\n • jwvsol@yandex.com'
 
 class Status(Enum):
     START = '0'  # Начало нового диалога
     LOGIN = '1'
-    NAME = '2'
-    PHONE = '3'
-    EMAIL = '4'
-    GENDER = '5'
-    PASSW = '6'
+    PASSW = '2'
+    NAME = '3'
+    PHONE = '4'
+    EMAIL = '5'
+    GENDER = '6'
+
